c175e645fefbe1f71c2f15203c730b8c185bc168
LuLuLuOfficial <xframilrainx@163.com>
2025-10-22 21:26:51 +0800

    忘记删除配置文件了, 另外给配置检查的自动创建里面加了 pyenv 的环境变量

--- src/pydt/_data_.py
+++ src/pydt/_data_.py
@@ -16,7 +16,7 @@ def data_get(path_workdir: str, path_python: str, path_pyScript: str, path_callB
             },
             "path": {
                 "python_standalone": f"{path_workdir}/python_standalone/python.exe",
-                "pyenv_win": f"{path_workdir}/pyenv_win",
+                "pyenv_win": f"{path_workdir}/pyenv_win/pyenv-win/bin/pyenv.bat",
                 "pipx": f"{path_workdir}/pipx/pipx-app.pyz"
             },
             "installed": {}
@@ -41,6 +41,13 @@ def data_get(path_workdir: str, path_python: str, path_pyScript: str, path_callB
                 "POETRY_DATA_DIR": "./pipx/home/venvs/poetry/pypoetry",
                 "POETRY_CACHE_DIR": "./pipx/home/venvs/poetry/pypoetry/cache",
                 "POETRY_VIRTUALENVS_IN_PROJECT": "true"
+            },
+            "pyenv-win": {
+                "PYENV": "/pyenv_win",
+                "PYENV_HOME": "/pyenv_win",
+                "PYENV_ROOT": "/pyenv_win",
+                "HTTP_PROXY": "http://127.0.0.1:7890",
+                "HTTPS_PROXY": "https://127.0.0.1:7890"
             }
         },
     }
